6484b4d512e33d4fb5bbb11c762ab7e3d10f3659
Hyewon Yoon <iey704@gachon.ac.kr>
2024-07-21 17:28:06 +0900

    feat: 헤더 생성(사원ver,관리자ver)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MemeberNavBar from './components/memberNavbar';
+import ManagerNavBar from './components/managerNavBar';
 
-function App() {
+const App: React.FC = () => {
+  const navItems = [
+    { title: '홈', path: '/' },
+    { title: '대시보드', path: '/dashboard' },
+  ];
+  const memberNavItems = [
+    { title: '마이페이지', path: '/myPage' },
+    { title: '위시리스트', path: '/wishList' },
+  ];
+  const managerNavItems = [
+    { title: '상품관리', path: '/manageGoods' },
+    { title: '사원관리', path: '/manageMember' },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MemeberNavBar navItems={navItems} memberNavItems={memberNavItems} path={''} />
+        {/* <ManagerNavBar navItems={navItems} managerNavItems={managerNavItems} path={''} /> */}
       </header>
+      <p className="App-page">Edit and save to reload.</p>
     </div>
   );
-}
+};
 
 export default App;
