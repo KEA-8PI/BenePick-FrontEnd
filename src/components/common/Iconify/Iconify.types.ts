bfffcc579ac6286fb082692b27b8e44070198b6f
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-07-22 23:23:44 +0900

    feat: iconify 컴포넌트 제작

---
+++ src/components/common/Iconify/Iconify.types.ts
@@ -0,0 +1,7 @@
+import { IconifyIcon } from '@iconify/react';
+import { BoxProps } from '@mui/material';
+
+export interface IconifyProps extends BoxProps {
+  icon: IconifyIcon | string;
+  width?: number;
+}
