6c3b7b388aa1267fe79ff80b043f857c11365573
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-07-22 23:27:50 +0900

    feat: Components.styles.ts 생성

---
+++ src/components/common/Components.styles.ts
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import colors from 'theme/variableColors';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+interface ShadowBoxProps {
+  width?: number;
+  height?: number;
+}
+
+export const ShadowBox = styled.div<ShadowBoxProps>`
+  display: flex;
+  width: ${({ width }: { width?: number }) => (width ? `${width}%` : 'auto')};
+  align-items: center;
+  height: ${({ height }: { height?: number }) => (height ? `${height}%` : 'auto')};
+  box-shadow: 0px 0px 10px 1px ${colors.tableGrey};
+  border-radius: 10px;
+  padding: 30px 0;
+  flex-direction: column;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: ${({ width }: { width?: number }) => (width ? `${width}%` : 'auto')};
+`;
