f02b920b2c36567aa1e1af83d0d7ddf48175501c
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-07-31 00:11:52 +0900

    style: SearchBar.styles.ts 파일 생성-OutlinedInput 컴포넌트화

---
+++ src/components/searchbar/SearchBar.styles.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import { TextField } from '@mui/material';
+import colors from 'theme/variableColors';
+
+export const OutlinedInput = styled(TextField)({
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: colors.primary,
+      borderRadius: '50px',
+      borderWidth: '2px',
+    },
+
+    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: colors.primary,
+    },
+    '&:hover fieldset': {
+      borderColor: colors.secondary,
+    },
+    '& .MuiInputBase-input': {
+      paddingLeft: '30px', // 원하는 padding 값을 설정합니다.
+    },
+  },
+});
