87d8fb8a1a6a043feaba3a5d354111a16639ce5c
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-07-31 00:12:53 +0900

    chore: CustomModal clickButton 함수 생성

--- src/components/CustomModal/CustomModal.tsx
+++ src/components/CustomModal/CustomModal.tsx
@@ -10,6 +10,13 @@ export const CustomModal = ({ modalConfig }: { modalConfig: IModalConfig }) => {
     return null;
   }
 
+  const clickButton = () => {
+    if (buttons) {
+      buttons.action();
+    }
+    onClose();
+  };
+
   return (
     <Modal
       open={open}
@@ -29,19 +36,21 @@ export const CustomModal = ({ modalConfig }: { modalConfig: IModalConfig }) => {
             justifyContent: 'center',
             alignItems: 'center',
             display: 'flex',
-            backgroundColor: 'grey',
+            // backgroundColor: 'grey',
             height: '63%',
           }}
         >
           {contents}
         </Box>
         {buttons ? (
-          <Stack direction="row" spacing={2} sx={{ justifyContent: 'space-around' }}>
+          <Stack direction="row" spacing={2} sx={{ justifyContent: 'space-around', padding: '0 7%' }}>
             <S.LeftButton onClick={onClose}>취소</S.LeftButton>
-            <S.RightButton onClick={buttons.action}>{buttons.label}</S.RightButton>
+            <S.RightButton onClick={clickButton}>{buttons.label}</S.RightButton>
           </Stack>
         ) : (
-          <S.RightButton onClick={close}>확인</S.RightButton>
+          <div style={{ justifyContent: 'center', display: 'flex' }}>
+            <S.RightButton onClick={onClose}>확인</S.RightButton>
+          </div>
         )}
       </S.Wrapper>
     </Modal>
