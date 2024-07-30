7079a976f204b008430cd1a75b125efb4d2dcc05
Hyewon Yoon <iey704@gachon.ac.kr>
2024-07-30 22:26:07 +0900

    feat: 파이 컴포넌트 구현

---
+++ src/components/pieChart/PieChart.tsx
@@ -0,0 +1,50 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
+const PieChart = ({ legendData, seriesData }) => {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const myChart = echarts.init(chartRef.current);
+      const option: echarts.EChartsOption = {
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          orient: 'horizontal',
+          left: 'center',
+          bottom: 0,
+          data: legendData,
+          width: '60%',
+        },
+        series: [
+          {
+            type: 'pie',
+            label: {
+              show: false,
+            },
+            radius: '70%',
+            center: ['50%', '40%'],
+            data: seriesData,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      };
+      myChart.setOption(option);
+      // Log the size of the chartRef element
+      const { width, height } = chartRef.current.getBoundingClientRect();
+      console.log(`Chart width: ${width}px, height: ${height}px`);
+    }
+  }, []);
+
+  return <div ref={chartRef} style={{ width: '100%', height: '100%' }} />;
+};
+
+export default PieChart;
