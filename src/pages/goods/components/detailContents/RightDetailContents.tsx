d2f59050481bde0b7a419241ea2e801f87518e98
Hyewon Yoon <iey704@gachon.ac.kr>
2024-07-30 14:12:18 +0900

    feat: 상품 상세 정보 컴포넌트 제작

---
+++ src/pages/goods/components/detailContents/RightDetailContents.tsx
@@ -0,0 +1,49 @@
+import * as S from 'components/common/Components.styles';
+import * as C from 'components/CustomCard/CustomCard.styles';
+import Iconify from 'components/common/Iconify/Iconify';
+import { Typography, IconButton, Button } from '@mui/material';
+import colors from 'theme/variableColors';
+
+const RightDetailContents = ({ info, like, handleLike }) => {
+  return (
+    <div>
+      <C.CardLightFont style={{ fontSize: '15px' }}>{info.category}</C.CardLightFont>
+      <S.Row>
+        <C.CardBoldFont style={{ fontSize: '25px', width: '350px' }}>{info.name}</C.CardBoldFont>{' '}
+        <div>
+          <IconButton>
+            <Iconify
+              icon={like ? 'gridicons:heart' : 'gridicons:heart-outline'}
+              onClick={handleLike}
+              color={like ? colors.primary : colors.grey01}
+              sx={{ width: '30px', height: '30px' }}
+            />
+          </IconButton>
+        </div>
+      </S.Row>
+      <C.CardBoldFont style={{ fontSize: '20px', textDecoration: 'line-through', color: colors.grey01 }}>
+        {info.price.toLocaleString()}원
+      </C.CardBoldFont>
+      <S.Row>
+        <C.CardBoldFont style={{ fontSize: '20px' }}>{info.discountPrice.toLocaleString()}원</C.CardBoldFont>
+        <Typography style={{ color: 'black', fontSize: '15px', paddingLeft: '150px' }}>{info.amounts}</Typography>
+      </S.Row>
+      <S.Row style={{ justifyContent: 'flex-end' }}>
+        <Button
+          style={{
+            width: '48px',
+            height: '22px',
+            fontSize: '15px',
+            backgroundColor: colors.whiteGrey,
+            color: colors.grey02,
+            marginTop: '30px',
+          }}
+        >
+          더보기
+        </Button>
+      </S.Row>
+    </div>
+  );
+};
+
+export default RightDetailContents;
