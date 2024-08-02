08f97c2ac205c0315ba249d699c89aebd6e1e836
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-03 00:48:38 +0900

    feat: 로그인 페이지 제작 및 관련 헤더 수정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import MemberNavBar from 'components/navbar/memberNavbar';
 import ManagerNavBar from 'components/navbar/managerNavBar';
+import { useLocation } from 'react-router-dom';
 // navItems 배열의 path 속성은 여전히 각 항목의 경로를 정의하는 데 필요
 const navItems = [
   { title: '홈', path: '/' },
@@ -16,11 +17,14 @@ const managerNavItems = [
 
 // Layout 컴포넌트에서 path Prop을 받아서, MemberNavBar 컴포넌트에 전달합니다.
 const Layout = (props: { children: React.ReactNode; path: string }) => {
+  const location = useLocation();
+  const role = location.state?.role || 'member'; // 일단 임시로 member로 설정
+
   return (
     <div style={{ padding: '130px 12%' }}>
       {/* <Header /> */}
-      <MemberNavBar navItems={navItems} memberNavItems={memberNavItems} path={props.path} />
-      {/* <ManagerNavBar navItems={navItems} managerNavItems={managerNavItems} path={props.path} /> */}
+      {role === 'member' && <MemberNavBar navItems={navItems} memberNavItems={memberNavItems} path={props.path} />}
+      {role === 'admin' && <ManagerNavBar navItems={navItems} managerNavItems={managerNavItems} path={props.path} />}
       <main>{props.children}</main>
       {/* <Footer /> */}
     </div>
