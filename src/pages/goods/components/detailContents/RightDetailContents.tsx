eeffff5bf92615d7f15ec8dac01b8d9f97da9efc
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-03 01:29:11 +0900

    feat:상품 상세페이지: 하트 아이콘 위치 수정

--- src/pages/goods/components/detailContents/RightDetailContents.tsx
+++ src/pages/goods/components/detailContents/RightDetailContents.tsx
@@ -1,37 +1,14 @@
 import * as S from 'components/common/Components.styles';
 import * as C from 'components/CustomCard/CustomCard.styles';
-import Iconify from 'components/common/Iconify/Iconify';
-import { Typography, IconButton, Button } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import colors from 'theme/variableColors';
 import { CustomCardData } from 'components/CustomCard/CustomCard.types';
 
-const RightDetailContents = ({
-  info,
-  like,
-  handleLike,
-}: {
-  info: CustomCardData;
-  like?: boolean;
-  handleLike?: () => void;
-}) => {
+const RightDetailContents = ({ info }: { info: CustomCardData; like?: boolean; handleLike?: () => void }) => {
   return (
     <div>
       <C.CardLightFont style={{ fontSize: '15px' }}>#{info.category}</C.CardLightFont>
-      <S.Row>
-        <C.CardBoldFont style={{ fontSize: '25px', width: '350px' }}>{info.name}</C.CardBoldFont>
-        {handleLike && (
-          <div>
-            <IconButton>
-              <Iconify
-                icon={like ? 'gridicons:heart' : 'gridicons:heart-outline'}
-                onClick={handleLike}
-                color={like ? colors.primary : colors.grey01}
-                sx={{ width: '30px', height: '30px' }}
-              />
-            </IconButton>
-          </div>
-        )}
-      </S.Row>
+      <C.CardBoldFont style={{ fontSize: '25px', width: '400px' }}>{info.name}</C.CardBoldFont>
       <C.CardBoldFont
         style={{ fontSize: '20px', textDecoration: 'line-through', color: colors.grey01, marginBottom: '3px' }}
       >
