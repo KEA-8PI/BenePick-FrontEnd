4c97519e03673ee6c841bb40161cd3d715ef5459
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-05 13:34:12 +0900

    feat: ConvertResponse 함수 제작

---
+++ src/utils/ConvertResponse.ts
@@ -0,0 +1,46 @@
+import colors from 'theme/variableColors';
+
+export const ConvertResponse = (response: string) => {
+  const ResultJson = {
+    WINNER: '당첨',
+    WAITLIST: '대기',
+    CANCEL: '취소',
+    NO_SHOW: '노쇼',
+    NON_WINNER: '미당첨',
+    PROGRESS: '진행중',
+    SCHEDULED: '예정',
+    COMPLETED: '결과 보기',
+  };
+  return ResultJson[response];
+};
+
+export const ConvertResponseColor = (response: string) => {
+  const ResultColor = {
+    WINNER: colors.mint,
+    NON_WINNER: colors.pinkGrey,
+    WAITLIST: colors.lemon,
+    CANCEL: colors.pinkGrey,
+    NO_SHOW: colors.buttonPink,
+  };
+  return ResultColor[response];
+};
+
+export const ConvertStatus = (status: string) => {
+  const StatusJson = {
+    CONFIRM: '확정',
+    WINNER: '확정대기',
+    CANCEL: '취소',
+    NO_SHOW: '노쇼',
+  };
+  return StatusJson[status];
+};
+
+export const ConvertStatusColor = (status: string) => {
+  const StatusColor = {
+    CONFIRM: colors.mint,
+    WINNER: colors.purple,
+    CANCEL: colors.pinkGrey,
+    NO_SHOW: colors.buttonPink,
+  };
+  return StatusColor[status];
+};
