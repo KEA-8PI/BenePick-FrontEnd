b48c43bdf63de830d5b426d8c68e1e7fe1f037cf
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-05 16:05:47 +0900

    feat: api 인스턴스 생성

---
+++ src/api/api.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+// import Cookies from 'js-cookie';
+
+export const baseUrl = () => {
+  // return import.meta.env.VITE_BASE_URL;
+  // return 'http://localhost:8080';
+  return 'http://benepick.kro.kr:10001';
+};
+
+const api = axios.create({
+  baseURL: 'http://benepick.kro.kr:10001/',
+  headers: {
+    common: {
+      'Content-Type': 'application/json',
+    },
+  },
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    // const accessToken = localStorage.getItem("accessToken");
+    // const tokenString = Cookies.get('token');
+    const tokenString =
+      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE3MjI4MjE2NTl9.b5EZncIInv3ujH4vSLqsV5-FhcQOahUcI2-qHo27RqM';
+    let accessToken = null;
+
+    if (tokenString) {
+      try {
+        const tokenData = JSON.parse(tokenString);
+        accessToken = tokenData.token;
+      } catch (e) {
+        console.error('Error parsing token from cookie:', e);
+      }
+    }
+    // console.log('accessToken:', accessToken);
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+  }
+  return config;
+});
+
+export default api;
