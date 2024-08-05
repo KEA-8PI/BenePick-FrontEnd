9cbf7e7bfd27ea236a033f536e2bc1cecb5a3dbd
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-05 14:13:47 +0900

    feat: WinnerTable 수정

--- src/pages/manageGoods/manageDrawResult/components/WinnerTable/WinnerTableRow.tsx
+++ src/pages/manageGoods/manageDrawResult/components/WinnerTable/WinnerTableRow.tsx
@@ -4,26 +4,60 @@ import TableCell from '@mui/material/TableCell';
 import colors from 'theme/variableColors';
 import { TableRowProps } from 'components/CustomTable/CustomTable.types';
 import { ColorBox } from 'components/common/Components.styles';
-
-const ResultJson = {
-  WINNER: '당첨',
-  WAITLIST: '확정대기', // 확인 필요
-  CANCEL: '취소',
-  NO_SHOW: '노쇼',
-};
-
-const ResultColor = {
-  당첨: colors.mint,
-  확정대기: colors.purple,
-  취소: colors.pinkGrey,
-  노쇼: colors.buttonPink,
-};
-
-const changeResult = () => {
-  // 팝오버
-};
+import { MenuItem, Popover, Typography } from '@mui/material';
+import { ConvertStatusColor, ConvertStatus } from 'utils/ConvertResponse';
+import Iconify from 'components/common/Iconify/Iconify';
+import CustomModal from 'components/CustomModal/CustomModal';
+import { useToggle } from 'hooks/useToggle';
 
 const WinnerTableRow: React.FC<TableRowProps> = ({ columns, index }) => {
+  const [status, setStatus] = React.useState<string>(columns[columns.length - 1].label.toString());
+  const changeToggle = useToggle();
+  const confirmToggle = useToggle();
+
+  const modalConfig = {
+    open: changeToggle.isOpen,
+    onClose: changeToggle.toggle,
+    contents: (
+      <>
+        <Typography variant="h6" color={colors.primary}>
+          {ConvertStatus(status)}
+        </Typography>
+        <Typography variant="h6">(으)로 변경하시겠습니까?</Typography>
+      </>
+    ),
+    buttons: { label: '확인', action: () => changeStatus() },
+  };
+
+  const confirmModalConfig = {
+    open: confirmToggle.isOpen,
+    onClose: confirmToggle.toggle,
+    contents: <Typography variant="h6">변경되었습니다.</Typography>,
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const changeStatus = () => {
+    // 당첨자 상태 관리 api 호출
+    confirmToggle.toggle();
+  };
+
+  const handleButtonClick = (status: string) => {
+    setStatus(status);
+    handleClose();
+    changeToggle.toggle();
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
     <TableRow hover tabIndex={-1} sx={{ '& .MuiTableCell-root': { paddingTop: '10px', paddingBottom: '10px' } }}>
       <TableCell>{index + 1}</TableCell>
@@ -31,15 +65,63 @@ const WinnerTableRow: React.FC<TableRowProps> = ({ columns, index }) => {
         <TableCell key={column.label} align={'center'}>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             {column.id === 'status' ? (
-              <ColorBox color={ResultColor[column.label]} onClick={() => changeResult()} style={{ cursor: 'pointer' }}>
-                {column.label}
-              </ColorBox>
+              <>
+                <ColorBox
+                  color={ConvertStatusColor(column.label.toString())}
+                  onClick={handleClick}
+                  style={{ cursor: 'pointer' }}
+                >
+                  {ConvertStatus(column.label.toString())}
+                  <Iconify icon="iconamoon:arrow-down-2-fill" sx={{ ml: 0.5 }} />
+                </ColorBox>
+                <Popover
+                  id={column.label.toString()}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                >
+                  <MenuItem onClick={() => handleButtonClick('CONFIRM')} disabled={status === 'CONFIRM'}>
+                    <>
+                      <Iconify icon="codicon:circle-filled" sx={{ mr: 2, color: colors.mint }} />
+                      확정
+                    </>
+                  </MenuItem>
+                  <MenuItem onClick={() => handleButtonClick('WINNER')} disabled={status === 'WINNER'}>
+                    <>
+                      <Iconify icon="codicon:circle-filled" sx={{ mr: 2, color: colors.purple }} />
+                      확정 대기
+                    </>
+                  </MenuItem>
+                  <MenuItem onClick={() => handleButtonClick('CANCEL')} disabled={status === 'CANCEL'}>
+                    <>
+                      <Iconify icon="codicon:circle-filled" sx={{ mr: 2, color: colors.pinkGrey }} />
+                      취소
+                    </>
+                  </MenuItem>
+                  <MenuItem onClick={() => handleButtonClick('NO_SHOW')} disabled={status === 'NO_SHOW'}>
+                    <>
+                      <Iconify icon="codicon:circle-filled" sx={{ mr: 2, color: colors.buttonPink }} />
+                      노쇼
+                    </>
+                  </MenuItem>
+                </Popover>
+              </>
             ) : (
               column.label
             )}
           </div>
         </TableCell>
       ))}
+      <CustomModal modalConfig={modalConfig} />
+      <CustomModal modalConfig={confirmModalConfig} />
     </TableRow>
   );
 };
