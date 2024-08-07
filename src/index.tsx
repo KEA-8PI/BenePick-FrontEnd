d6cd703e27338f42d55bcd4a7026f63edfafd974
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-07 16:28:37 +0900

    feat: redux 폴더 생성

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
+import store from 'reducer/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
