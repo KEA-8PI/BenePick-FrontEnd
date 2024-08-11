fb4373a6e3c98a99da5b7aa0f5e04addf7837114
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-11 21:49:05 +0900

    feat: 사원 관리 테이블 페이징 적용, 상품 업로드 실패 시 alert 추가

--- src/pages/manageMember/view/ManageMemberView.tsx
+++ src/pages/manageMember/view/ManageMemberView.tsx
@@ -2,27 +2,25 @@ import { IconButton, InputAdornment, Typography } from '@mui/material';
 import * as S from 'components/common/Components.styles';
 import Iconify from 'components/common/Iconify/Iconify';
 import UploadBox from 'components/uploadBox/UploadBox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OutlinedInput } from 'components/searchbar/SearchBar.styles';
 import CustomSelectTable from 'components/CustomSelectTable/CustomSelectTable';
 import CustomModal from 'components/CustomModal/CustomModal';
 import { useToggle } from 'hooks/useToggle';
-import { PostMemberUpload, PostMemberPointUpload } from 'api/memberAdmin.api';
+import { PostMemberUpload, PostMemberPointUpload, GetMemberList, DeleteMember } from 'api/memberAdmin.api';
 
 const ManageMemberView = () => {
   const deleteToggle = useToggle();
   const confirmToggle = useToggle();
 
   const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('');
   const [selected, setSelected] = useState([]);
   const [isModify, setIsModify] = useState('');
 
-  const [rowData, setRowData] = useState([
-    { id: 'alex.js', deptName: '서비스 개발팀', name: '김밤비', point: 5, penaltyCnt: 0 },
-    { id: 'bamb.kim', deptName: '서비스 개발팀', name: '김감기', point: 5, penaltyCnt: 2 },
-    { id: 'hello.js', deptName: '서비스 개발팀', name: '김미소', point: 5, penaltyCnt: 3 },
-  ]);
+  const [rowDataNum, setRowDataNum] = useState(0);
+
+  const [rowData, setRowData] = useState([]);
+  const [apiPage, setApiPage] = useState(0);
 
   const modalConfig = {
     open: deleteToggle.isOpen,
@@ -40,20 +38,24 @@ const ManageMemberView = () => {
   const handlePointSubmit = (formData: FormData) => {
     PostMemberPointUpload(formData)
       .then((res) => {
-        console.log('Post 멤버 복지포인트 response:', res);
+        // console.log('Post 멤버 복지포인트 response:', res);
+        window.location.reload();
       })
       .catch((err) => {
         console.error('Post 멤버 복지포인트 error:', err);
+        alert('복지포인트 추가에 실패했습니다. 올바른 형식을 사용했는지 확인해주세요.');
       });
   };
 
   const handleMemberSubmit = (formData: FormData) => {
     PostMemberUpload(formData)
       .then((res) => {
-        console.log('Post 멤버 추가 response:', res);
+        // console.log('Post 멤버 추가 response:', res);
+        window.location.reload();
       })
       .catch((err) => {
         console.error('Post 멤버 추가 error:', err);
+        alert('사원 업르드에 실패했습니다. 올바른 형식을 사용했는지 확인해주세요.');
       });
   };
 
@@ -65,7 +67,40 @@ const ManageMemberView = () => {
 
   const handleDeleteMember = () => {
     //사원 삭제 api 호출
+    DeleteMember(selected)
+      .then((res) => {
+        console.log('Delete 멤버 response:', res);
+        setRowData(rowData.filter((row) => !selected.includes(row.id)));
+        setSelected([]);
+      })
+      .catch((err) => {
+        console.error('Delete 멤버 error:', err);
+      });
     confirmToggle.toggle();
+  };
+
+  const fetchMemberList = (apiPage: number, searchTerm = '') => {
+    GetMemberList(apiPage, 25, searchTerm)
+      .then((res) => {
+        console.log('Get 멤버 리스트 response:', res.data.result);
+        // const newList = res.data.result.membersDetailResponseDTOList;
+        apiPage === 0
+          ? setRowData(res.data.result.membersDetailResponseDTOList)
+          : setRowData((prevRowData) => [...prevRowData, ...res.data.result.membersDetailResponseDTOList]);
+        apiPage === 0 && setRowDataNum(res.data.result.totalCnt);
+      })
+      .catch((err) => {
+        console.error('Get 멤버 리스트 error:', err);
+      });
+  };
+
+  useEffect(() => {
+    fetchMemberList(apiPage, search);
+  }, [apiPage, search]);
+
+  const handleSearch = () => {
+    setApiPage(0); // Reset to first apiPage on new search
+    fetchMemberList(0, search);
   };
 
   return (
@@ -85,7 +120,7 @@ const ManageMemberView = () => {
             onChange={(e) => setSearch(e.target.value)}
             onKeyDown={(event) => {
               if (event.key === 'Enter') {
-                setQuery(search);
+                handleSearch();
               }
             }}
             placeholder="성명을 입력하세요"
@@ -98,7 +133,7 @@ const ManageMemberView = () => {
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
-                  <IconButton aria-label="toggle password visibility" onClick={() => setQuery(search)}>
+                  <IconButton aria-label="toggle password visibility" onClick={() => handleSearch()}>
                     <Iconify icon="eva:search-fill" sx={{ width: 25, height: 25, color: 'black' }} />
                   </IconButton>
                 </InputAdornment>
@@ -121,6 +156,9 @@ const ManageMemberView = () => {
           setRowData={setRowData}
           selected={selected}
           setSelected={setSelected}
+          totalNum={rowDataNum}
+          apiPage={apiPage}
+          setApiPage={setApiPage}
         />
         <S.Row style={{ width: '100%', justifyContent: 'flex-end', marginTop: '20px' }}>
           <S.CustomButton sx={{ mr: '1%' }} onClick={() => deleteToggle.toggle()} disabled={selected.length === 0}>
