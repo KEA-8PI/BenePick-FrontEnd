989c04a1fc9af0778dd11dba5667b5280230d765
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-12 17:40:09 +0900

    feat: 복지 포인트 데이터 전달

--- src/pages/goods/components/button/ButtonView.tsx
+++ src/pages/goods/components/button/ButtonView.tsx
@@ -2,10 +2,10 @@ import RaffleButton from './RaffleButton';
 import RaffleBeforeButton from './RaffleBeforeButton';
 import RaffleResultButton from './RaffleResultButton';
 
-const ButtonView = ({ info, goodsStatus }) => {
+const ButtonView = ({ info, goodsStatus, point }) => {
   return (
     <div>
-      {goodsStatus === 'PROGRESS' && <RaffleButton info={info} />}
+      {goodsStatus === 'PROGRESS' && <RaffleButton info={info} point={point} />}
       {goodsStatus === 'SCHEDULED' && <RaffleBeforeButton />}
       {goodsStatus === 'COMPLETED' && <RaffleResultButton info={info} />}
     </div>
