72b22b78c7ec45c1039d6b100857c352f607d72a
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-15 21:39:53 +0900

    chore: 결과 발표 멘트 수정

--- src/pages/goods/components/tab/drawOutcome/DrawOutcomeIng.tsx
+++ src/pages/goods/components/tab/drawOutcome/DrawOutcomeIng.tsx
@@ -11,8 +11,6 @@ const DrawOutcomeIng = forwardRef<HTMLElement, DrawOutcomeProps>(({ info }, ref)
   return (
     <S.Wrapper style={{ paddingTop: '20px', width: '100%', justifyContent: 'center' }}>
       <S.ShadowBox style={{ width: 600, height: 300, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        응모 해주셔서 감사합니다.
-        <br />
         결과 발표는 {convertISOtoKST(info.raffleEndAt)} 이후에 발표될 예정입니다.
       </S.ShadowBox>
     </S.Wrapper>
