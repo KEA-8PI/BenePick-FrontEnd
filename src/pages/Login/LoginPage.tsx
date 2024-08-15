2053d5d05558a71fb768c1711e7cbc8e1858edd5
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-15 13:20:14 +0900

    fix: 로그인 했을 때 로그인 페이지 접근 에러 처리

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,16 @@
 import { Helmet } from 'react-helmet-async';
 import { LoginView } from './view';
+import { useEffect } from 'react';
+import { useAccountStore } from 'store/useAccountStore';
 
 const LoginPage = () => {
+  const { accountInfo } = useAccountStore();
+  const { role } = accountInfo;
+
+  useEffect(() => {
+    if (role !== '') window.location.href = '*';
+  }, [role]);
+
   return (
     <>
       <Helmet>
