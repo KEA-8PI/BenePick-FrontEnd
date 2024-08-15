1546c86f45f82c7a47f1b5e332c3d5c4899ea689
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-16 00:56:27 +0900

    feat: 배포된 주소로 반영

--- src/api/http.api.ts
+++ src/api/http.api.ts
@@ -1,15 +1,10 @@
 import axios from 'axios';
 
 export const baseUrl = () => {
-  // return import.meta.env.VITE_BASE_URL;
-  // return 'http://benepick.kro.kr:10001/';
-  // return 'http://localhost:8080/';
-  return 'https://benepick.o-r.kr/';
+  return 'https://backend.benepick.kro.kr/';
 };
 
 const httpApi = axios.create({
-  // baseURL: 'http://benepick.kro.kr:10001/',
-  // baseURL: 'http://localhost:8080/',
   baseURL: 'https://backend.benepick.kro.kr/',
   headers: {
     common: {
