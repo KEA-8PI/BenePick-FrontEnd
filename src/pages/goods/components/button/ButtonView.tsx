719c4ebc8106dd5365bc7af32fd6da706de21255
Hyewon Yoon <iey704@gachon.ac.kr>
2024-08-15 16:02:56 +0900

    feat: 관리자 계정에서는 응모하기 no show

--- src/pages/goods/components/button/ButtonView.tsx
+++ src/pages/goods/components/button/ButtonView.tsx
@@ -1,11 +1,13 @@
 import RaffleButton from './RaffleButton';
 import RaffleBeforeButton from './RaffleBeforeButton';
 import RaffleResultButton from './RaffleResultButton';
+import { useAccountStore } from 'store/useAccountStore';
 
 const ButtonView = ({ info, goodsStatus, point }) => {
+  const userRole = useAccountStore((state) => state.accountInfo.role);
   return (
     <div>
-      {goodsStatus === 'PROGRESS' && <RaffleButton info={info} point={point} />}
+      {userRole === 'MEMBER' && goodsStatus === 'PROGRESS' && <RaffleButton info={info} point={point} />}
       {goodsStatus === 'SCHEDULED' && <RaffleBeforeButton />}
       {goodsStatus === 'COMPLETED' && <RaffleResultButton info={info} />}
     </div>
